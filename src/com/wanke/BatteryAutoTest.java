5ecc365478f1a2fe997c4e8b14dd4d2ae07bceaf
staroftheair <staroftheair@613.com>
2016-03-07 03:41:12 +0800

    PLC通信

--- src/com/wanke/BatteryAutoTest.java
+++ src/com/wanke/BatteryAutoTest.java
@@ -1,24 +1,13 @@
 package com.wanke;
 
-import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.jdom.Element;
-import org.jdom.output.XMLOutputter;
 import org.junit.Test;
-
-import com.sun.jna.platform.win32.Kernel32;
-import com.sun.jna.platform.win32.WinBase;
-import com.sun.jna.platform.win32.WinNT;
-import com.sun.jna.platform.win32.WinDef.ULONGByReference;
 import com.sun.jna.platform.win32.WinNT.HANDLE;
-import com.sun.jna.ptr.IntByReference;
 import com.wanke.bean.neware.NewareInquireBean;
 import com.wanke.bean.neware.NewareInquireResultBean;
 import com.wanke.bean.neware.NewareStartBean;
@@ -33,15 +22,7 @@ public class BatteryAutoTest {
 	public static final String IP_ADDR = "192.168.1.2";// 服务器地址
 	public static final int PORT = 9600;// 服务器端口号
 
-	@Test
-	public void testGetStr() {
-		String aa = PlcTransUtil.GetPlcHandshakeHexStr(12, 250);
-
-		System.out.println(aa);
-	}
-
-	@Test
-	public void testNamedPipeServerAPI() {
+	public void NewareStart(Socket socket) {
 		NewareTrans nt = new NewareTrans();
 		HANDLE pipe = nt.ConnectNewareAPI();
 		XmlUtil xu = new XmlUtil();
@@ -114,41 +95,31 @@ public class BatteryAutoTest {
 	}
 
 	@Test
-	public void testcc() {
+	public void PlcTest() {
 		Socket socket = null;
 		try {
 			socket = new Socket(IP_ADDR, PORT);
-
+			PlcTransUtil plcutil = new PlcTransUtil();
 			while (true) {
-				// 读取服务器端数据
+				// PLC握手
 				DataInputStream input = new DataInputStream(socket.getInputStream());
-				// 向服务器端发送数据
 				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-				byte[] b = UtilStr.hexStr2Bytes("46494E530000000C0000000000000000000000FA");
-				out.write(b);
+				String handshakeStr = plcutil.GetPlcHandshakeHexStr(12, 250);
+				byte[] handshake = handshakeStr.getBytes();
+				out.write(handshake);
 				out.flush();
-				int i = 1;
-				byte[] a = new byte[50];
-				if (input.read(a) != -1) {
-					i++;
-					String aa = UtilStr.byte2HexStr(a);
-					if (a[23] == 2) {
+				byte[] handshakeResuletb = new byte[50];
+				if (input.read(handshakeResuletb) != -1) {
+					String handshakeResulet = UtilStr.byte2HexStr(handshakeResuletb);
+					String handshakeResuletback = plcutil.GetHandshakeBackHexStr(16, 1, 0, 2, 250);
+					if (handshakeResulet.indexOf(handshakeResuletback) > 0) {
 						while (true) {
-							// 读取服务器端数据
-							input = new DataInputStream(socket.getInputStream());
-							// 向服务器端发送数据
-							out = new DataOutputStream(socket.getOutputStream());
-							byte[] bb = UtilStr.hexStr2Bytes(
-									"46494E530000001B000000020000000080000200020000FA00FF010231000A05000101");
-							// byte[] bb = UtilStr.hexStr2Bytes(
-							// "46494E530000001C000000020000000080000200020000FA00FF0102820064000001FFFF");
-							a = new byte[1024];
-							out.write(bb);
-							out.flush();
-							int res = input.read(a);
-							if (res != -1) {
-								aa = UtilStr.byte2HexStr(a);
-								System.out.print(aa + "\r\n");
+							// 检测启动位
+							String plccommand = plcutil.SetReadOrWriteStr(26, 2, 0, 2, 250, 0, "W", 10, 1, 1, 0);
+							String plccommandback = plcutil.PlcTrans(socket, plccommand);
+							String plccommandCheck = plcutil.SetReadOrWriteStrBack(27, 2, 0, 2, 250, 0, 1);
+							if (plccommandback.indexOf(plccommandCheck) > 0) {
+								NewareStart(socket);
 							}
 						}
 
@@ -156,12 +127,9 @@ public class BatteryAutoTest {
 				}
 			}
 
-		} catch (UnknownHostException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			testcc();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			testcc();
+			PlcTest();
 		} finally {
 			if (socket != null) {
 				try {
--- src/com/wanke/util/PlcTransUtil.java
+++ src/com/wanke/util/PlcTransUtil.java
@@ -4,13 +4,27 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
-
-import com.wanke.bean.ConnectInfo;
 
 public class PlcTransUtil {
 
 	private static String PLC_HEAD = "FINS";
+
+	public String PlcTrans(Socket socket, String plccommand) throws IOException {
+
+		DataInputStream input = new DataInputStream(socket.getInputStream());
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		byte[] commandByte = UtilStr.hexStr2Bytes(plccommand);
+		byte[] resultByte = new byte[1024];
+		out.write(commandByte);
+		out.flush();
+		int res = input.read(commandByte);
+		String result = "";
+		if (res != -1) {
+			result = UtilStr.byte2HexStr(resultByte);
+			System.out.print(result + "\r\n");
+		}
+		return result;
+	}
 
 	/**
 	 * 获取握手字符串
@@ -21,13 +35,13 @@ public class PlcTransUtil {
 	 *            终端IP最后一位
 	 * @return String 握手用HEX字符串
 	 */
-	public static String GetPlcHandshakeHexStr(int length, int clientNo) {
+	public String GetPlcHandshakeHexStr(int length, int clientNo) {
 		String handshake = "";
 		handshake += UtilStr.str2HexStr(PLC_HEAD);
-		handshake += UtilStr.byte2HexStr(IntByteUtil.intToBytes2(length));
-		handshake += UtilStr.byte2HexStr(IntByteUtil.intToBytes2(0));
-		handshake += UtilStr.byte2HexStr(IntByteUtil.intToBytes2(0));
-		handshake += UtilStr.byte2HexStr(IntByteUtil.intToBytes2(clientNo));
+		handshake += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(length));
+		handshake += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(0));
+		handshake += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(0));
+		handshake += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(clientNo));
 
 		return handshake;
 	}
@@ -47,58 +61,113 @@ public class PlcTransUtil {
 	 *            终端IP最后一位
 	 * @return String 握手用返回HEX字符串
 	 */
-	public static String GetHandshakeBackHexStr(int length, int comandno, int errorno, int plcno, int clientno) {
+	public String GetHandshakeBackHexStr(int length, int comandno, int errorno, int plcno, int clientno) {
 		String handshakeback = "";
 		handshakeback += UtilStr.str2HexStr(PLC_HEAD);
-		handshakeback += UtilStr.byte2HexStr(IntByteUtil.intToBytes2(length));
-		handshakeback += UtilStr.byte2HexStr(IntByteUtil.intToBytes2(comandno));
-		handshakeback += UtilStr.byte2HexStr(IntByteUtil.intToBytes2(errorno));
-		handshakeback += UtilStr.byte2HexStr(IntByteUtil.intToBytes2(clientno));
-		handshakeback += UtilStr.byte2HexStr(IntByteUtil.intToBytes2(plcno));
+		handshakeback += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(length));
+		handshakeback += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(comandno));
+		handshakeback += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(errorno));
+		handshakeback += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(clientno));
+		handshakeback += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(plcno));
 
 		return handshakeback;
 	}
 
-	public static String GetReadPLCStr() {
-		String status = "";
-		return status;
-	}
-
-	public static String GetWritePLCStr() {
-		String status = "";
-		return status;
-	}
-
-	public ConnectInfo ConnectPLC(String ADDR_IP, String PORT, String client_last_IP, String PLC_last_IP) {
-		ConnectInfo conni = new ConnectInfo();
-		Socket socket = null;
-
-		try {
-			while (true) {
-				// 读取服务器端数据
-				DataInputStream input = new DataInputStream(socket.getInputStream());
-				// 向服务器端发送数据
-				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-
-			}
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			System.out.println("PLC连接 异常:" + e.getMessage());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println("PLC连接 异常::" + e.getMessage());
-		} finally {
-			if (socket != null) {
-				try {
-					socket.close();
-				} catch (IOException e) {
-					socket = null;
-					System.out.println("客户端 finally 异常:" + e.getMessage());
-				}
-			}
+	/**
+	 * 设置读取写入16进制字符串 例：将PLC的W3.10置ON
+	 * 46494E530000001B0000000200000000800002000100000200FF01023100030A000101
+	 * （1B:数据长度；01：PLC节点号；02：电脑节点号；0102：FINS命令码，表示写；31：W(Bit）区；0003：十六进制，这里是第3通道
+	 * ；0A：十六进制，这里是第10位；0001：十六进制，通道数1个；01：要写入的数据1）
+	 * 
+	 * @param length
+	 *            发送数据长度
+	 * @param comandno
+	 *            命令码
+	 * @param errorno
+	 *            错误码
+	 * @param plcno
+	 *            PLCIP
+	 * @param clientno
+	 *            客户机IP
+	 * @param rw
+	 *            0:读取;1:写入
+	 * @param scope
+	 *            区
+	 * @param channel
+	 *            通道
+	 * @param bit
+	 *            位数
+	 * @param channelcount
+	 *            通道数
+	 * @param value
+	 *            设置值
+	 * @return
+	 */
+	public String SetReadOrWriteStr(int length, int comandno, int errorno, int plcno, int clientno, int rw,
+			String scope, int channel, int bit, int channelcount, int value) {
+		String transStr = "";
+		transStr += UtilStr.str2HexStr(PLC_HEAD);
+		transStr += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(length));
+		transStr += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(comandno));
+		transStr += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(errorno));
+		transStr += "80000200";
+		transStr += UtilStr.byte2HexStr(IntByteUtil.intToBytes(plcno)) + "0000"
+				+ UtilStr.byte2HexStr(IntByteUtil.intToBytes(clientno));
+		if (rw == 0) {
+			transStr += "00FF0101";
+		} else {
+			transStr += "00FF0102";
 		}
-		return conni;
+		transStr += UtilStr.str2HexStr(scope) + "00" + UtilStr.byte2HexStr(IntByteUtil.intToBytes(channel))
+				+ UtilStr.byte2HexStr(IntByteUtil.intToBytes(bit));
+		transStr += UtilStr.byte2HexStr(IntByteUtil.intToBytes2(channelcount));
 
+		if (rw != 0) {
+			transStr += UtilStr.byte2HexStr(IntByteUtil.intToBytes(value));
+		}
+
+		return transStr;
+	}
+
+	/**
+	 * 正确返回收到的响应： 46494E53000000160000000200000000C00002000200000100FF01020000
+	 * 
+	 * @param length
+	 *            发送数据长度
+	 * @param comandno
+	 *            命令码
+	 * @param errorno
+	 *            错误码
+	 * @param plcno
+	 *            PLCIP
+	 * @param clientno
+	 *            客户机IP
+	 * @param rw
+	 *            0:读取;1:写入
+	 * @return
+	 */
+	public String SetReadOrWriteStrBack(int length, int comandno, int errorno, int plcno, int clientno, int rw,
+			int value) {
+		String transStrBack = "";
+		transStrBack += UtilStr.str2HexStr(PLC_HEAD);
+		transStrBack += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(length));
+		transStrBack += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(comandno));
+		transStrBack += UtilStr.byte2HexStr(IntByteUtil.intToBytes4(errorno));
+		transStrBack += "C0000200";
+		transStrBack += UtilStr.byte2HexStr(IntByteUtil.intToBytes(clientno)) + "0000"
+				+ UtilStr.byte2HexStr(IntByteUtil.intToBytes(plcno));
+
+		if (rw == 0) {
+			transStrBack += "00FF0101";
+		} else {
+			transStrBack += "00FF0102";
+		}
+		transStrBack += "0000";
+
+		if (rw == 0) {
+			transStrBack += UtilStr.byte2HexStr(IntByteUtil.intToBytes(value));
+		}
+		return transStrBack;
 	}
 
 }
